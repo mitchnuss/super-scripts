44bb751d3ea1f4bca89c43d5ccb0ee7619603aba
mitchnuss <mitch.nuss@gmail.com>
2019-02-06 00:11:07 -0600

    added logging

--- usage.sh
+++ usage.sh
@@ -5,4 +5,5 @@ free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 
 df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
 top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
 sleep 5
+while true; do uptime >> uptime.log: sleep 1; done
 done
